ec19a35f9caa49dc1ee67b581ef0cee357fd1da8
Daniel López <daniel.lopez.lopez@juntadeandalucia.es>
2018-06-04 15:29:43 +0200

    nueva rama que usa librería MenuSystem

--- src/main.h
+++ src/main.h
@@ -2,19 +2,10 @@
 #include <PID_v1.h>
 #include <PID_AutoTune_v0.h>
 #include <RunningMedian.h>
-#include <U8g2lib.h>
 #include <EEPROM.h>
-#include <LCDMenuLib2.h>    // see https://github.com/Jomelo/LCDMenuLib2
-
-// U8g2lib
-#include <U8g2lib.h>
-
-#ifdef U8X8_HAVE_HW_SPI
-  #include <SPI.h>
-#endif
-  #ifdef U8X8_HAVE_HW_I2C
-  #include <Wire.h>
-#endif
+#include <MenuSystem.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
 
 
 //##############################################################################
@@ -49,16 +40,10 @@
 #define PIN_LCD_DC      5
 #define PIN_LCD_DIN     6
 #define PIN_LCD_SCLK    7
-#define BUTTON_RIGHT    _LCDML_CONTROL_digital_enter
-#define BUTTON_UP       _LCDML_CONTROL_digital_up
-#define BUTTON_DOWN     _LCDML_CONTROL_digital_down
+#define BUTTON_ENTER    10
+#define BUTTON_DOWN     11
 #define PIN_PWM         12
 #define PIN_THM         A0
-
-#define _LCDML_CONTROL_digital_enter           8
-#define _LCDML_CONTROL_digital_up              9
-#define _LCDML_CONTROL_digital_down            10
-#define _LCDML_CONTROL_digital_quit            11
 
 //EEPROM
 #define ADDR_FIRM_VERSION 1
@@ -66,77 +51,16 @@
 
 
 
-// LCD pin out
-#define PIN_LCD_RST     3
-#define PIN_LCD_CS      4
-#define PIN_LCD_DC      5
-#define PIN_LCD_DIN     6
-#define PIN_LCD_SCLK    7
-
-// configuración de los botones de control
-#define _LCDML_CONTROL_digital_low_active      0  // 0 = high active (pulldown) button, 1 = low active (pullup)
-#define _LCDML_CONTROL_digital_enable_quit     0  //No se usa botón quit
-#define _LCDML_CONTROL_digital_enable_lr       0  //No se usan boton left right
-#define _LCDML_CONTROL_digital_enter           8
-#define _LCDML_CONTROL_digital_up              9
-#define _LCDML_CONTROL_digital_down            10
-#define _LCDML_CONTROL_digital_quit            11 //No se usa
-#define _LCDML_CONTROL_digital_left            12 //No se usa
-#define _LCDML_CONTROL_digital_right           13 //No se usa
-
-
-// settings for lcd
-#define _LCDML_DISP_cols  20
-#define _LCDML_DISP_rows  4
-
-// settings for u8g lib and lcd
-#define _LCDML_DISP_w                 84             // lcd width
-#define _LCDML_DISP_h                 48             // lcd height
-// font settings
-#define _LCDML_DISP_font              u8g_font_6x13  // u8glib font (more fonts under u8g.h line 1520 ...)
-#define _LCDML_DISP_font_w            6              // font width
-#define _LCDML_DISP_font_h            13             // font heigt
-// cursor settings
-#define _LCDML_DISP_cursor_char       "X"            // cursor char
-#define _LCDML_DISP_cur_space_before  2              // cursor space between
-#define _LCDML_DISP_cur_space_behind  4              // cursor space between
-// menu position and size
-#define _LCDML_DISP_box_x0            0              // start point (x0, y0)
-#define _LCDML_DISP_box_y0            0              // start point (x0, y0)
-#define _LCDML_DISP_box_x1            84             // width x  (x0 + width)
-#define _LCDML_DISP_box_y1            48             // hight y  (y0 + height)
-#define _LCDML_DISP_draw_frame        1              // draw a box around the menu
- // scrollbar width
-#define _LCDML_DISP_scrollbar_w       6  // scrollbar width (if this value is < 3, the scrollbar is disabled)
-// nothing change here
-#define _LCDML_DISP_cols_max          ((_LCDML_DISP_box_x1-_LCDML_DISP_box_x0)/_LCDML_DISP_font_w)
-#define _LCDML_DISP_rows_max          ((_LCDML_DISP_box_y1-_LCDML_DISP_box_y0-((_LCDML_DISP_box_y1-_LCDML_DISP_box_y0)/_LCDML_DISP_font_h))/_LCDML_DISP_font_h)
-// rows and cols
-// when you use more rows or cols as allowed change in LCDMenuLib.h the define "_LCDML_DISP_cfg_max_rows" and "_LCDML_DISP_cfg_max_string_length"
-// the program needs more ram with this changes
-
-// menu element count - last element id
-// this value must be the same as the last menu element
-#define _LCDML_DISP_cnt    8
-
 
 //##############################################################################
 //                            F U N C T I O N S
 //##############################################################################
 double adc2temp(int adc, float Vin, float sr);
 double adc2temp(int adc, float Vin, float sr, boolean vccTherm);
-
-void setupLCD();
-void lcdml_menu_display();
-void lcdml_menu_clear();
-void lcdml_menu_control();
-void mDyn_time(uint8_t line);
-void mDyn_temp(uint8_t line);
-void mFunc_start(uint8_t param);
-void mFunc_setPID(uint8_t param);
-void mDyn_Kp(uint8_t line);
-void mDyn_Ki(uint8_t line);
-void mDyn_Kd(uint8_t line);
+void on_start_selected(MenuItem* p_menu_component);
+void on_setTime_selected(NumericMenuItem* p_menu_component);
+void on_setTemp_selected(NumericMenuItem* p_menu_component);
+void on_autoPID_selected(MenuItem* p_menu_component);
 
 
 //##############################################################################
@@ -173,16 +97,9 @@ double pidInput, pidOutput;
 unsigned long serialTime, lcdTime, pidTime, tempTime;
 
 // control del menú
-boolean showMenu;
+boolean showMenu, saveToEeprom;
 
-//extern Adafruit_PCD8544                display;
-PID           myPID (&pidInput, &pidOutput, &pidData.datos.pidSetPoint, DEFAULT_KP, DEFAULT_KI, DEFAULT_KD, DIRECT);//REVERSE;
-PID_ATune     aTune (&pidInput, &pidOutput);
-RunningMedian temp  (100);
-//extern U8G2_PCD8544_84X48_1_4W_SW_SPI  u8g2;
-
-LCDMenuLib2_menu                LCDML_0 (255, 0, 0, NULL, NULL); // root menu element (do not change)
-LCDMenuLib2                     LCDML   (LCDML_0, _LCDML_DISP_rows, _LCDML_DISP_cols, lcdml_menu_display, lcdml_menu_clear, lcdml_menu_control);
-U8G2_PCD8544_84X48_1_4W_SW_SPI  u8g2    (U8G2_R0, PIN_LCD_SCLK, PIN_LCD_DIN, PIN_LCD_CS, PIN_LCD_DC, PIN_LCD_RST); // The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
-
-unsigned long g_LCDML_DISP_press_time = 0;
+PID               myPID (&pidInput, &pidOutput, &pidData.datos.pidSetPoint, DEFAULT_KP, DEFAULT_KI, DEFAULT_KD, DIRECT);//REVERSE;
+PID_ATune         aTune (&pidInput, &pidOutput);
+RunningMedian     temp  (100);
+Adafruit_PCD8544  lcd   (PIN_LCD_SCLK, PIN_LCD_DIN, PIN_LCD_CS, PIN_LCD_DC, PIN_LCD_RST);
