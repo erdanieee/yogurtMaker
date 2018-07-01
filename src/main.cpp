bac49b1de3006345fb1c09e082501c4d7db0f8d2
Er Danié <erdanieee@gmail.com>
2018-07-01 16:09:55 +0200

    intentando hacer funcionar la pantalla

--- src/main.cpp
+++ src/main.cpp
@@ -88,8 +88,8 @@ MyRenderer my_renderer;
 MenuSystem      ms (my_renderer);
 MenuItem        mm_mi1("Empezar"      , &on_start_selected);
 NumericMenuItem mm_mi2("Tiempo"       , &on_setTime_selected, DEFAULT_TIME, 1, 24, 1);
-NumericMenuItem mm_mi3("Temperatura"  , &on_setTemp_selected, DEFAULT_TEMP, 20, 50, 1);
-MenuItem        mm_mi4("Ajuste PID"   , &on_autoPID_selected);
+//NumericMenuItem mm_mi3("Temp. "       , &on_setTemp_selected, DEFAULT_TEMP, 20, 50, 1);
+//MenuItem        mm_mi4("Ajuste PID"   , &on_autoPID_selected);
 
 
 
@@ -103,8 +103,8 @@ void setup() {
   Serial.println("init...");
 
   lcd.begin();
-  lcd.setContrast(60);
-  lcd.clearDisplay();
+  lcd.setContrast(50);
+  lcd.display();
 
   // set PIN modes
   pinMode(BUTTON_ENTER, INPUT_PULLUP);
@@ -113,7 +113,8 @@ void setup() {
   pinMode(PIN_PWM,      OUTPUT);
 
   // Si la EEPROM tiene una versión compatible, se cargan los valores
-  if (EEPROM.read(ADDR_FIRM_VERSION) >= FIRM_VERSION){
+  if (EEPROM.read(ADDR_FIRM_VERSION) == FIRM_VERSION){
+
     Serial.println("restoring EEPROM values");
     for( unsigned int i=0 ; i<sizeof(PIDDATA) ; i++  ){
       pidData.b[i] = EEPROM.read( ADDR_PID_DATA+i );
@@ -137,14 +138,20 @@ void setup() {
   //construye menu
   ms.get_root_menu().add_item(&mm_mi1);
   ms.get_root_menu().add_item(&mm_mi2);
-  ms.get_root_menu().add_item(&mm_mi3);
-  ms.get_root_menu().add_item(&mm_mi4);
+  //ms.get_root_menu().add_item(&mm_mi3);
+  //ms.get_root_menu().add_item(&mm_mi4);
 
-  showMenu    = true;
-  tempTime    = 0;
-  lcdTime     = 0;
-  serialTime  = 0;
-  pidTime     = 0;
+  aTune.SetOutputStep(10);
+  aTune.SetControlType(1);
+  aTune.SetNoiseBand(0.8);
+  aTune.SetLookbackSec(20);
+
+  fermentation = false;
+  autoTunning  = false;
+  tempTime     = 0;
+  lcdTime      = 0;
+  serialTime   = 0;
+  pidTime      = 0;
 }
 
 
@@ -155,26 +162,42 @@ void setup() {
 //                                   L O O P
 //##############################################################################
 void loop() {
+  if(digitalRead(BUTTON_ENTER)==LOW){
+    Serial.println("Select button pressed");
+    ms.select();
+    ms.display();
+    delay(100);
+  }
+
+  if(digitalRead(BUTTON_DOWN)==LOW){
+    Serial.println("Down button pressed");
+    ms.next(true);
+    delay(100);
+  }
+
   //lee el termistor
   if(millis() > tempTime){
     temp.add(analogRead(PIN_THM));
     tempTime = millis() + TIME_UPDATE_TEMP;
   }
 
-  //calcula pidOutput a partir de pidInput y ajuta PWM en concordancia
-  if(!showMenu && millis() > pidTime){
-      pidInput = adc2temp(temp.getMedian(), VCC, SERIESRESISTOR);
+  //calcula pidOutput a partir de pidInput y ajusta PWM en concordancia
+  if(millis() > pidTime){
+    pidInput = adc2temp(temp.getMedian(), VCC, SERIESRESISTOR, true);
+    pidTime = millis() + TIME_UPDATE_PID;
+  }
+
+  if((fermentation || autoTunning) && millis() > pidTime){
+    if (!autoTunning){
       myPID.Compute();
-      analogWrite(PIN_PWM, pidOutput);
-      pidTime = millis() + TIME_UPDATE_PID;
+    }
+    analogWrite(PIN_PWM, pidOutput);
+    pidTime = millis() + TIME_UPDATE_PID;
   }
 
   //update LCD if needed
   if(millis() > lcdTime) {
-    if(showMenu){
-      ms.display();
-
-    } else{
+    if (fermentation){
       lcd.setTextSize(2);
       lcd.setCursor(0, 0);
       lcd.print("Temp: ");
@@ -194,6 +217,31 @@ void loop() {
       lcd.print(s);
 
       lcd.display();
+
+    } else if (autoTunning){
+      lcd.setCursor(0,0);
+
+      char buf[20];
+      sprintf (buf, "Kp: %2.1f", pidData.datos.pidKp);
+      lcd.println(buf);
+
+      sprintf (buf, "Ki: %2.1f", pidData.datos.pidKi);
+      lcd.println(buf);
+
+      sprintf (buf, "Kd: %2.1f", pidData.datos.pidKd);
+      lcd.println(buf);
+
+      sprintf (buf, "Set: %2.1f", pidData.datos.pidSetPoint);
+      lcd.println(buf);
+
+      sprintf (buf, "Temp: %2.1f",  adc2temp(temp.getMedian(), VCC, SERIESRESISTOR));
+      lcd.println(buf);
+
+      lcd.display();
+
+    } else {
+      ms.display();
+
     }
 
     lcdTime = millis() + TIME_REFRESH_LCD;
@@ -207,12 +255,12 @@ void loop() {
     serialTime = millis() + TIME_REFRESH_SERIAL;
   }
 
-  if(millis() > fermData.endTime){
+  if(fermentation && millis() > fermData.endTime){
     Serial.print("Fermentación terminada!");
-    showMenu = true;
+    fermentation = false;
     analogWrite(PIN_PWM, 0);
 
-    while (!showMenu) {
+    while (digitalRead(BUTTON_ENTER)==HIGH && digitalRead(BUTTON_DOWN)==HIGH) {
       if(millis() > lcdTime) {
         lcd.setCursor(0, 0);
         lcd.setTextSize(3);
@@ -220,15 +268,25 @@ void loop() {
         lcd.display();
         lcdTime = millis() + TIME_REFRESH_LCD;
       }
-
-      if (digitalRead(BUTTON_ENTER)==LOW || digitalRead(BUTTON_DOWN)==LOW){
-        showMenu = true;
-        delay(100);
-      }
     }
+    delay(100);
+  }
+
+  if (autoTunning && aTune.Runtime() != 0){
+    Serial.println("Autotunning terminado");
+    autoTunning = false;
+
+    pidData.datos.pidKp = aTune.GetKp();
+    pidData.datos.pidKi = aTune.GetKi();
+    pidData.datos.pidKd = aTune.GetKd();
+    for( unsigned int i=0 ; i<sizeof(PIDDATA) ; i++  ) {
+      EEPROM.write( ADDR_PID_DATA+i , pidData.b[i] );
+    }
+    EEPROM.write(ADDR_FIRM_VERSION, FIRM_VERSION);
+    myPID.SetTunings(pidData.datos.pidKp,pidData.datos.pidKi,pidData.datos.pidKd);
+    Serial.print(" Kp: "); Serial.print(pidData.datos.pidKp); Serial.print(" pidKi: "); Serial.print(pidData.datos.pidKi); Serial.print(" Kd: "); Serial.println(pidData.datos.pidKd);
   }
 }
-
 
 
 
@@ -252,20 +310,35 @@ double adc2temp(int adc, float Vin, float sr, boolean vccTherm){
       radc = sr*(Vin-vadc)/vadc;
   }
 
+
+  steinhart = radc / THERMISTORNOMINAL;        // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+
+  /*
   rinf = THERMISTORNOMINAL*exp(-BCOEFFICIENT/TEMPERATURENOMINAL);               //R0*e^(-B/T0)
   steinhart = BCOEFFICIENT/log(radc/rinf);                                      //T = B/log(Rout/Rinf)
   steinhart -= 273.15;                                                          // convert to C
+  */
 
   return steinhart;
 }
+/*#define THERMISTORNOMINAL   100000.0  // resistance at 25 degrees C
+#define TEMPERATURENOMINAL  25.0      // temp. for nominal resistance (almost always 25 C)
+#define BCOEFFICIENT        3950.0    // The beta coefficient of the thermistor (usually 3000-4000)
+#define SERIESRESISTOR      98700.0   // the value of the 'other' resistor
+#define VCC                 5.0*/
 
 
 
-
-void on_start_selected(MenuItem* p_menu_component){
+void on_start_selected(MenuComponent* p_menu_component){
+  Serial.println("Start fermentation");
   fermData.startTime = millis();
   fermData.endTime   = fermData.startTime + (fermData.timeSet*3600*1000);
-  showMenu=false;
+  fermentation = true;
   //TODO: grabar en eeprom si ha habido modificación de parámetros
 }
 
@@ -280,63 +353,6 @@ void on_setTemp_selected(NumericMenuItem* p_menu_component){
 }
 
 void on_autoPID_selected(MenuItem* p_menu_component){
+  autoTunning = true;
   Serial.println("autotuning...");
-
-  aTune.SetOutputStep(10);
-  aTune.SetControlType(1);
-  aTune.SetNoiseBand(0.8);
-  aTune.SetLookbackSec(20);
-
-  //lee el termistor
-  tempTime  = 0;
-  lcdTime   = 0;
-  pidTime   = 0;
-  while(aTune.Runtime() == 0){
-    if(millis() > tempTime){
-      temp.add(analogRead(PIN_THM));
-      tempTime = millis() + TIME_UPDATE_TEMP;
-    }
-
-    if(millis() > lcdTime) {
-      lcd.setCursor(0,0);
-
-      char buf[20];
-      sprintf (buf, "Kp: %2.1f", pidData.datos.pidKp);
-      lcd.println(buf);
-
-      sprintf (buf, "Ki: %2.1f", pidData.datos.pidKi);
-      lcd.println(buf);
-
-      sprintf (buf, "Kd: %2.1f", pidData.datos.pidKd);
-      lcd.println(buf);
-
-      sprintf (buf, "Set: %2.1f", pidData.datos.pidSetPoint);
-      lcd.println(buf);
-
-      sprintf (buf, "Temp: %2.1f",  adc2temp(temp.getMedian(), VCC, SERIESRESISTOR));
-      lcd.println(buf);
-
-      lcdTime = millis() + TIME_REFRESH_LCD;
-    }
-
-    if(millis() > pidTime){
-        pidInput = adc2temp(temp.getMedian(), VCC, SERIESRESISTOR);
-        analogWrite(PIN_PWM, pidOutput);
-        pidTime = millis() + TIME_UPDATE_PID;
-    }
-  }
-
-  pidData.datos.pidKp = aTune.GetKp();
-  pidData.datos.pidKi = aTune.GetKi();
-  pidData.datos.pidKd = aTune.GetKd();
-  for( unsigned int i=0 ; i<sizeof(PIDDATA) ; i++  ) {
-    EEPROM.write( ADDR_PID_DATA+i , pidData.b[i] );
-  }
-  EEPROM.write(ADDR_FIRM_VERSION, FIRM_VERSION);
-  myPID.SetTunings(pidData.datos.pidKp,pidData.datos.pidKi,pidData.datos.pidKd);
-
-  Serial.println("Autotunning terminado");
-  Serial.print(" Kp: "); Serial.print(pidData.datos.pidKp); Serial.print(" pidKi: "); Serial.print(pidData.datos.pidKi); Serial.print(" Kd: "); Serial.println(pidData.datos.pidKd);
-
-  resetFunc();  //reboot!!
 }
--- src/main.h
+++ src/main.h
@@ -12,7 +12,7 @@
 //                              C O N S T A N T S
 //##############################################################################
 // firmware version
-#define FIRM_VERSION  1
+#define FIRM_VERSION  2
 
 // PID default settings
 #define DEFAULT_TEMP  42  //Recomendado entre 37 y 46ºC
@@ -22,27 +22,27 @@
 #define DEFAULT_KD    2
 
 // update time (refresh, read ADC, ...)
-#define TIME_REFRESH_LCD      50
+#define TIME_REFRESH_LCD      500
 #define TIME_REFRESH_SERIAL   5000
 #define TIME_UPDATE_PID       1000
 #define TIME_UPDATE_TEMP      100
 
 // thermistor constants
-#define THERMISTORNOMINAL   100000  // resistance at 25 degrees C
-#define TEMPERATURENOMINAL  25      // temp. for nominal resistance (almost always 25 C)
-#define BCOEFFICIENT        3950    // The beta coefficient of the thermistor (usually 3000-4000)
-#define SERIESRESISTOR      98700   // the value of the 'other' resistor
-#define VCC                 5
+#define THERMISTORNOMINAL   100000.0  // resistance at 25 degrees C
+#define TEMPERATURENOMINAL  25.0      // temp. for nominal resistance (almost always 25 C)
+#define BCOEFFICIENT        3950.0    // The beta coefficient of the thermistor (usually 3000-4000)
+#define SERIESRESISTOR      98700.0   // the value of the 'other' resistor
+#define VCC                 5.0
 
 //pin out
-#define PIN_LCD_RST     3
-#define PIN_LCD_CS      4
-#define PIN_LCD_DC      5
-#define PIN_LCD_DIN     6
-#define PIN_LCD_SCLK    7
+#define PIN_LCD_RST     5
+#define PIN_LCD_CS      6
+#define PIN_LCD_DC      7
+#define PIN_LCD_DIN     8
+#define PIN_LCD_SCLK    9
 #define BUTTON_ENTER    10
 #define BUTTON_DOWN     11
-#define PIN_PWM         12
+#define PIN_PWM         4
 #define PIN_THM         A0
 
 //EEPROM
@@ -57,10 +57,11 @@
 //##############################################################################
 double adc2temp(int adc, float Vin, float sr);
 double adc2temp(int adc, float Vin, float sr, boolean vccTherm);
-void on_start_selected(MenuItem* p_menu_component);
+void on_start_selected  (MenuComponent* p_menu_component);
+void on_autoPID_selected(MenuItem* p_menu_component);
 void on_setTime_selected(NumericMenuItem* p_menu_component);
 void on_setTemp_selected(NumericMenuItem* p_menu_component);
-void on_autoPID_selected(MenuItem* p_menu_component);
+
 
 
 //##############################################################################
@@ -97,9 +98,10 @@ double pidInput, pidOutput;
 unsigned long serialTime, lcdTime, pidTime, tempTime;
 
 // control del menú
-boolean showMenu, saveToEeprom;
+boolean fermentation, saveToEeprom, autoTunning; //TODO: meter fermetation y autoTunning en un struct para hacer estados
 
 PID               myPID (&pidInput, &pidOutput, &pidData.datos.pidSetPoint, DEFAULT_KP, DEFAULT_KI, DEFAULT_KD, DIRECT);//REVERSE;
 PID_ATune         aTune (&pidInput, &pidOutput);
 RunningMedian     temp  (100);
-Adafruit_PCD8544  lcd   (PIN_LCD_SCLK, PIN_LCD_DIN, PIN_LCD_CS, PIN_LCD_DC, PIN_LCD_RST);
+Adafruit_PCD8544  lcd   (PIN_LCD_SCLK, PIN_LCD_DIN, PIN_LCD_DC, PIN_LCD_CS, PIN_LCD_RST);
+//Adafruit_PCD8544 lcd = Adafruit_PCD8544(9, 8, 7, 6, 5);
